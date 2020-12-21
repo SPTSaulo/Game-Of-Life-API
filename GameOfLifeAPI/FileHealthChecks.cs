b4ea5ddd479e3c6a37f16ef04cfec84c1ea7db71
Saulo Santana Batista <sausantana@domingoalonsogroup.com>
2020-12-21 11:11:16 +0000

    Removed Directory Exist

--- GameOfLifeAPI/FileHealthChecks.cs
+++ GameOfLifeAPI/FileHealthChecks.cs
@@ -7,30 +7,30 @@ using System.Threading.Tasks;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
-namespace GameOfLifeAPI {
+namespace GameOfLifeAPI
+{
     public class FileHealthChecks : IHealthCheck {
         private readonly IConfiguration configuration;
         private readonly ILogger _loger;
         private string directoryPath;
 
-        public FileHealthChecks(IConfiguration configuration)
-        {
+        public FileHealthChecks(IConfiguration configuration) {
             this.configuration = configuration;
             directoryPath = configuration["DirectoryLogs"];
         }
-        public Task<HealthCheckResult> CheckHealthAsync(HealthCheckContext context, CancellationToken cancellationToken = default)
-        {
-            if (Directory.Exists(directoryPath)) {
-                try {
-                    var filePath = configuration["DirectoryLogs"] + "\\dummyLogs.txt";
-                    File.AppendAllText(filePath, "Escribo");
-                    File.Delete(filePath);
-                    return Task.FromResult(HealthCheckResult.Healthy("Healthy result"));
-                } catch (Exception e) {
-                    return Task.FromResult(HealthCheckResult.Unhealthy("A unhealthy result! (File does not have permissions)"));
-                }
+
+        public Task<HealthCheckResult> CheckHealthAsync(HealthCheckContext context,
+            CancellationToken cancellationToken = default) {
+            try {
+                var filePath = configuration["DirectoryLogs"] + "\\dummyLogs.txt";
+                File.AppendAllText(filePath, "Escribo");
+                File.Delete(filePath);
+                return Task.FromResult(HealthCheckResult.Healthy("Healthy result"));
             }
-            return Task.FromResult(HealthCheckResult.Unhealthy($"A unhealthy result! (File does not exist {directoryPath})"));
+            catch (Exception e) {
+                return Task.FromResult(
+                    HealthCheckResult.Unhealthy("A unhealthy result! (File does not have permissions)"));
+            }
         }
     }
 }
